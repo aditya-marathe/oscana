5cb196ddbd25557f289ad5c232132db08fe1f418
Aditya Marathe <72881014+aditya-marathe@users.noreply.github.com>
2025-01-23 17:20:58 +0000

    Added '__init__.py': Keeping things organised.

---
+++ src/oscana/data/__init__.py
@@ -0,0 +1,12 @@
+"""
+oscana / data
+
+--------------------------------------------------------------------------------
+
+Author - Aditya Marathe
+Email  - aditya.marathe.20@ucl.ac.uk
+
+--------------------------------------------------------------------------------
+"""
+
+from .sntp_loader import SNTPFileLoader
