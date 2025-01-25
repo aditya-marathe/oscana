d278ed84d355a71883dc15c321207a55b6ae1323
Aditya Marathe <72881014+aditya-marathe@users.noreply.github.com>
2025-01-25 22:30:04 +0000

    Updated '__init__.py': Changed the imports to reflect the updates made to 'metadata.py'.

--- src/oscana/data/__init__.py
+++ src/oscana/data/__init__.py
@@ -10,4 +10,4 @@ Email  - aditya.marathe.20@ucl.ac.uk
 """
 
 from .data_loader import FileLoader
-from .functions import SNTP_BR_NTPST, SNTP_BR_NTPBDLITE
+from .functions import SNTP_BR_STD, SNTP_BR_BDL, SNTP_BR_FIT
