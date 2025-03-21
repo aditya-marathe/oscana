a2dac4fb015c6c5d1a038fc7630233c7a8a72e08
Aditya Marathe <72881014+aditya-marathe@users.noreply.github.com>
2025-03-21 10:55:38 +0000

    Moved `OscanaError` to this module from 'utils.py'.

---
+++ src/oscana/errors.py
@@ -0,0 +1,16 @@
+"""\
+oscana / errors.py
+
+--------------------------------------------------------------------------------
+
+Author - Aditya Marathe
+Email  - aditya.marathe.20@ucl.ac.uk
+
+--------------------------------------------------------------------------------
+
+This module contains all the error classes used in the package.
+"""
+
+
+class OscanaError(Exception):
+    pass
