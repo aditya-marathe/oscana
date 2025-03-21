81abe7ddcc064200ea4122557c2583116becafe6
Aditya Marathe <72881014+aditya-marathe@users.noreply.github.com>
2025-03-21 11:46:13 +0000

    Implemented `_get_non_cache_files` to prevent re-loading of files when re-running cells in IPYNBs.

--- src/oscana/data/io_base.py
+++ src/oscana/data/io_base.py
@@ -12,16 +12,18 @@ from __future__ import annotations
 
 __all__ = []
 
-from typing import TYPE_CHECKING, TypeVar, TypeAlias, Callable, Generic
+from typing import TYPE_CHECKING, TypeAlias, TypeVar, Callable, Generic
 
+import logging
 from pathlib import Path
 from abc import ABC, abstractmethod
 
 from .f_metadata import FileMetadata
 from .t_metadata import TransformMetadata
+from ..utils import _error
 
 if TYPE_CHECKING:
-    from .data_handler import DataHandler  # Guard against circular imports.
+    from .data_handler import DataHandler
 
 # ============================= [ Type Aliases ] ============================= #
 
@@ -44,6 +46,45 @@ LoadedDataType: TypeAlias = tuple[
     T, list[FileMetadata], TransformMetadata | None
 ]
 
+# =============================== [ Logging  ] =============================== #
+
+logger = logging.getLogger("Root")
+
+# =========================== [ Helper Functions ] =========================== #
+
+
+def _get_non_cache_files(cache: list[str], files: list[str]) -> list[str]:
+    """\
+    [ Internal ]
+
+    Get the files that are not in the cache.
+
+    Parameters
+    ----------
+    cache : list[str]
+        List of files that are already in the cache.
+
+    files : list[str]
+        List of files to check against the cache.
+
+    Returns
+    -------
+    list[str]
+        List of files that are not in the cache.
+    """
+    non_cache_files = []
+
+    for file in files:
+        if file in cache:
+            logger.info(f"Skipping '{file}' as it is already in the cache.")
+            continue
+
+        non_cache_files.append(file)
+
+    cache.extend(non_cache_files)
+
+    return non_cache_files
+
 
 # =========================== [ Data IO Strategy ] =========================== #
 
@@ -58,6 +99,7 @@ class DataIOStrategy(ABC, Generic[T]):  # Can't use the cool 3.12 syntax :(
 
     def __init__(self, parent: DataHandler) -> None:
         self._parent: DataHandler = parent
+        self._cache: list[str] = []
 
     @abstractmethod
     def _init_data_table(self) -> T:
@@ -88,20 +130,86 @@ class DataIOStrategy(ABC, Generic[T]):  # Can't use the cool 3.12 syntax :(
         }
 
     @abstractmethod
+    def _from_sntp(self, files: list[str]) -> None:
+        pass
+
+    @abstractmethod
+    def _from_udst(self, files: list[str]) -> None:
+        pass
+
+    @abstractmethod
+    def _from_hdf5(self, files: list[str]) -> None:
+        pass
+
     def from_sntp(self, files: list[str]) -> None:
-        pass
+        """\
+        Load data from SNTP ROOT files.
 
-    @abstractmethod
+        Parameters
+        ----------
+        files : list[str]
+            List of names of the SNTP ROOT files.  
+
+        Notes
+        -----
+        The names of the files should be from the environment variables (i.e. 
+        the '.env' file).
+        """
+        files = _get_non_cache_files(self._cache, files)
+
+        if not files:
+            return
+
+        return self._from_sntp(files=files)
+
     def from_udst(self, files: list[str]) -> None:
-        pass
+        """\
+        Load data from uDST ROOT files.
 
-    @abstractmethod
+        Parameters
+        ----------
+        files : list[str]
+            List of names of the uDST ROOT files.
+
+        Notes
+        -----
+        The names of the files should be from the environment variables (i.e.
+        the '.env' file).
+        """
+        files = _get_non_cache_files(self._cache, files)
+
+        if not files:
+            return
+
+        return self._from_udst(files=files)
+
     def from_hdf5(self, files: list[str]) -> None:
-        pass
+        """\
+        Load data from HDF5 files.
 
-    @abstractmethod
+        Parameters
+        ----------
+        files : list[str]
+            List of names of the HDF5 files.
+
+        Notes
+        -----
+        The names of the files should be from the environment variables (i.e.
+        the '.env' file).
+        """
+        files = _get_non_cache_files(self._cache, files)
+
+        if not files:
+            return
+
+        return self._from_hdf5(files=files)
+
     def to_hdf5(self, file: str | Path) -> None:
-        pass
+        _error(
+            NotImplementedError,
+            "The HDF5 writer is not implemented yet!",
+            logger,
+        )
 
     def __str__(self) -> str:
         return (
