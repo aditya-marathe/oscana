473a4b7b3205c3168d1a224ba4d429f3c140c78e
Aditya Marathe <72881014+aditya-marathe@users.noreply.github.com>
2026-09-03 12:37:08 +0100

    Updated avalible themes - added some themes for my Neutrino2026 along with some new fonts (to somewhat match NOvA's plotting theme). Am I allowed to commit these fonts to my GitHub? - Not sure...

--- src/oscana/themes.py
+++ src/oscana/themes.py
@@ -40,10 +40,15 @@ MINIMUM_TEXT_SIZE: Final[int] = 15
 CMU_BRIGHT_SEMIBOLD: Final[str] = "cmunbsr.ttf"
 CMU_TYPEWRITER_TEXT_REGULAR: Final[str] = "cmuntt.ttf"
 
+HELVITICA_RM: Final[str] = "arialr.ttf"
+HELVITICA_IT: Final[str] = "ariali.ttf"
+HELVITICA_BF: Final[str] = "arialb.ttf"
+HELVITICA_BF_IT: Final[str] = "arialbi.ttf"
+
 # =========================== [ Theme Dataclass  ] =========================== #
 
 
-@dataclass(slots=True, frozen=True)
+@dataclass(frozen=True)
 class Theme:
     """\
     Dataclass for plotting themes. Only for astheics!
@@ -73,6 +78,7 @@ class Theme:
 
     edge_colour: str
     face_colour: str
+    axes_colour: str
     text_colour: str
     colour_cycle: list[str]
 
@@ -82,8 +88,15 @@ class Theme:
     text_size: int
 
     # Image
-
+    
     cmap: str
+
+    # Math Text
+
+    math_font_rm: str = HELVITICA_RM
+    math_font_it: str = HELVITICA_IT
+    math_font_bf: str = HELVITICA_BF
+    math_font_bf_it: str = HELVITICA_BF_IT
 
     def get_cycler(self) -> cycler:
         """\
@@ -171,6 +184,11 @@ def _load_settings(theme_name: str) -> dict[str, Any]:
 
     default_font = _load_font(font_name=theme.text_font)
 
+    math_font_rm = _load_font(font_name=theme.math_font_rm)
+    math_font_it = _load_font(font_name=theme.math_font_it)
+    math_font_bf = _load_font(font_name=theme.math_font_bf)
+    math_font_bf_it = _load_font(font_name=theme.math_font_bf_it)
+
     return {
         # Quality
         "figure.dpi": 130,
@@ -185,7 +203,7 @@ def _load_settings(theme_name: str) -> dict[str, Any]:
         "figure.titlesize": int(theme.text_size * 1.1),
         "figure.autolayout": True,  # Not sure if this will break anything...
         # Axes
-        "axes.facecolor": theme.face_colour,
+        "axes.facecolor": theme.axes_colour,
         "axes.edgecolor": theme.edge_colour,
         "axes.prop_cycle": theme.get_cycler(),
         "axes.titlelocation": "left",
@@ -194,8 +212,11 @@ def _load_settings(theme_name: str) -> dict[str, Any]:
         "axes.labelpad": 10,
         "axes.labelcolor": theme.text_colour,
         "axes.formatter.use_mathtext": True,
+        "axes.formatter.limits": (-3, 3),
         "axes.xmargin": 0.0,
         "axes.ymargin": 0.3,
+        # Grid
+        "grid.alpha": 0.3,
         # Axis
         "xaxis.labellocation": "center",
         "yaxis.labellocation": "center",
@@ -204,6 +225,17 @@ def _load_settings(theme_name: str) -> dict[str, Any]:
         "font.size": theme.text_size,
         "font.family": default_font,
         "font.stretch": "semi-expanded",
+        # Math Text
+        "mathtext.fontset": "custom",
+        "mathtext.sf": math_font_rm,
+        "mathtext.rm": math_font_rm,
+        "mathtext.it": f"{math_font_it}:italic",
+        "mathtext.bf": f"{math_font_bf}:bold",
+        "mathtext.tt": "monospace",
+        "mathtext.cal": "DejaVu Sans",
+        "mathtext.bfit": f"{math_font_bf_it}:bold:italic",
+        "mathtext.fallback": "cm",
+        "mathtext.default": "it",
         # X-Ticks
         "xtick.top": True,
         "xtick.bottom": True,
@@ -212,7 +244,7 @@ def _load_settings(theme_name: str) -> dict[str, Any]:
         "xtick.major.size": 7,
         "xtick.minor.size": 3.5,
         "xtick.color": theme.edge_colour,
-        "xtick.labelcolor": theme.edge_colour,
+        "xtick.labelcolor": theme.text_colour,
         # Y-Ticks
         "ytick.left": True,
         "ytick.right": True,
@@ -221,18 +253,21 @@ def _load_settings(theme_name: str) -> dict[str, Any]:
         "ytick.major.size": 7,
         "ytick.minor.size": 3.5,
         "ytick.color": theme.edge_colour,
-        "ytick.labelcolor": theme.edge_colour,
+        "ytick.labelcolor": theme.text_colour,
         # Linestyle
         "lines.dashed_pattern": (7, 4),
+        "lines.linewidth": 1.5,
+        "lines.antialiased": True,
         # Image
         "image.cmap": theme.cmap,
         "image.origin": "lower",
         "image.aspect": "auto",
         # Legend
         "legend.loc": "upper right",
-        "legend.frameon": False,
+        "legend.frameon": True,
         "legend.framealpha": 0.4,
         "legend.facecolor": theme.face_colour,
+        "legend.edgecolor": theme.face_colour,
         "legend.borderpad": 0.5,
         "legend.fontsize": int(theme.text_size * 0.9),
         "legend.labelcolor": theme.text_colour,
@@ -244,9 +279,9 @@ def _load_settings(theme_name: str) -> dict[str, Any]:
 # Note: `_colour_cycle` is temporary and it may be replaced with a JSON file in
 #       the resources folder.
 
-_colour_cycle = {
+_colour_cycles = {
     "BrickPlot": [
-        # A twist on the classic Matplotlib theme.
+        # A LEGO twist on the classic Matplotlib theme.
         "#0055BF",  # Blue
         "#FE8A18",  # Orange
         "#237841",  # Green
@@ -259,7 +294,7 @@ _colour_cycle = {
         "#008F9B",  # Dark Turquoise
     ],
     "SandBrickPlot": [
-        # A pastel twist on the classic Matplotlib theme.
+        # A pastel LEGO twist on the classic Matplotlib theme.
         "#6074A1",  # Sand Blue
         "#FA9C1C",  # Earth Orange
         "#C7D23C",  # Medium Lime
@@ -271,14 +306,25 @@ _colour_cycle = {
         "#73DCA1",  # Medium Green
         "#55A5AF",  # Light Turquoise
     ],
+    "ROOT": [
+        "#1845FB", # kP8Blue
+        "#FF5e02", # kP8Orange
+        "#C91F16", # kP8Red
+        "#C849A9", # kP8Pink
+        "#ADAD7D", # kP8Green
+        "#86C8DD", # kP8Cyan
+        "#578DFF", # kP8Azure
+        "#656364", # kP8Gray
+    ],
 }
 
 themes = {
     "slate": Theme(
         edge_colour="#FFFFFF",  # White
         face_colour="#1E1E1E",  # Slate
+        axes_colour="#1E1E1E",  # Slate
         text_colour="#FFFFFF",  # White
-        colour_cycle=_colour_cycle["BrickPlot"],
+        colour_cycle=_colour_cycles["BrickPlot"],
         text_font=CMU_BRIGHT_SEMIBOLD,
         text_size=MINIMUM_TEXT_SIZE,
         cmap="magma",
@@ -286,8 +332,9 @@ themes = {
     "sandyslate": Theme(
         edge_colour="#FFFFFF",  # White
         face_colour="#1E1E1E",  # Slate
+        axes_colour="#1E1E1E",  # Slate
         text_colour="#FFFFFF",  # White
-        colour_cycle=_colour_cycle["SandBrickPlot"],
+        colour_cycle=_colour_cycles["SandBrickPlot"],
         text_font=CMU_BRIGHT_SEMIBOLD,
         text_size=MINIMUM_TEXT_SIZE,
         cmap="magma",
@@ -295,8 +342,9 @@ themes = {
     "light": Theme(
         edge_colour="#000000",  # Black
         face_colour="#FFFFFF",  # White
+        axes_colour="#FFFFFF",  # White
         text_colour="#000000",  # Black
-        colour_cycle=_colour_cycle["BrickPlot"],
+        colour_cycle=_colour_cycles["BrickPlot"],
         text_font=CMU_BRIGHT_SEMIBOLD,
         text_size=MINIMUM_TEXT_SIZE,
         cmap="magma_r",
@@ -304,10 +352,41 @@ themes = {
     "draft": Theme(
         edge_colour="#000000",
         face_colour="#D9D9D9",
+        axes_colour="#D9D9D9",
         text_colour="#000000",
-        colour_cycle=_colour_cycle["BrickPlot"],
+        colour_cycle=_colour_cycles["BrickPlot"],
         text_font=CMU_TYPEWRITER_TEXT_REGULAR,
         text_size=MINIMUM_TEXT_SIZE,
         cmap="magma_r",
+    ),
+    "nova": Theme(
+        edge_colour="#000000",  # Black
+        face_colour="#FFFFFF",  # White
+        axes_colour="#FFFFFF",  # White
+        text_colour="#000000",  # Black
+        colour_cycle=_colour_cycles["ROOT"],
+        text_font=HELVITICA_RM,
+        text_size=20,
+        cmap="cividis",
+    ),
+    "nova-nu26-darkblue": Theme(
+        edge_colour="#000000",  # Black
+        face_colour="#0D79CA",  # Dark Blue
+        axes_colour="#FFFFFF",  # White
+        text_colour="#FFFFFF",  # White
+        colour_cycle=_colour_cycles["ROOT"],
+        text_font=HELVITICA_RM,
+        text_size=20,
+        cmap="cividis",
+    ),
+    "nova-nu26-lightblue": Theme(
+        edge_colour="#000000",  # Black
+        face_colour="#ECF6FE",  # Light Blue
+        axes_colour="#FFFFFF",  # White
+        text_colour="#000000",  # Black
+        colour_cycle=_colour_cycles["ROOT"],
+        text_font=HELVITICA_RM,
+        text_size=20,
+        cmap="cividis",
     ),
 }
